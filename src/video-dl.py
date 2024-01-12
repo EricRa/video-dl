6836c4956cb78a00c4178a6c04d516a8fa5cebe0
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2024-01-11 19:47:37 -0500

    Start looking into subprocess.run logic

--- src/video-dl.py
+++ src/video-dl.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+import sys
+import subprocess
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
@@ -7,6 +9,21 @@ app = ctk.CTk()
 app.title("video-dl")
 app.geometry("800x500")
 app.resizable(False, False)
+
+# Code here to find installation directory of yt-dlp
+#
+#
+#
+
+# Code here to assign str(filepath) to a variable to use later
+#
+#
+#
+
+# Function definition here for the subprocess.call to invoke yt-dlp
+#
+#
+#
 
 
 # Input box for video URL
@@ -20,12 +37,20 @@ video_url.insert("0.0", "Paste URL of video here")
 #video_url_output = video_url.get("0.0", "end") # Use later to get text
 
 
+# Textbox for file path (where to save file..default = downloads?)
+# Button to select this directory
+
+
+# Output window for yt-dlp output
 output_window = ctk.CTkTextbox(
     app,
     width = 700,
     height = 150,
 )    
 output_window.insert("0.0", "Output will be shown here")
+
+
+
 
 
 # Layout
