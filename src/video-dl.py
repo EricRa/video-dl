e5e36ae6479d5a6be55e0316d37ef8c6bdb66c5d
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2024-01-19 13:44:55 -0500

    Ruff check and format

--- src/video-dl.py
+++ src/video-dl.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import sys
 
@@ -26,10 +25,11 @@ testing_video = ""
 video_link = ""
 console_output = []
 
+
 def get_downloads_folder():
     """
-    Tries to locate the user's Downloads folder in their home directory.  
-    
+    Tries to locate the user's Downloads folder in their home directory.
+
     Only need to run this if user wants files downloaded there instead of
     the directory where the script was run.
     """
@@ -41,6 +41,7 @@ def get_downloads_folder():
             ic("Download folder found")
     except:
         ic("Download folder not found")
+
 
 def call_yt_dlp(video_link, *args):
     """
@@ -57,8 +58,10 @@ def call_yt_dlp(video_link, *args):
     we can print those out.
 
     """
-    
-    result = subprocess.run(["yt-dlp", video_link, *args], capture_output=True, text=True)
+
+    result = subprocess.run(
+        ["yt-dlp", video_link, *args], capture_output=True, text=True
+    )
 
     console_output.append(result.stdout)
     console_output.append(result.stderr)
@@ -79,16 +82,16 @@ def dl_button_press():
     """
 
     url = video_url.get("0.0", "end")  # gets URL from video_url text box
-    command_args = [] #arguments to pass to yt-dlp
+    command_args = []  # arguments to pass to yt-dlp
 
     ic("Download button pressed")
-    
+
     if sub_checkbox.get() == "on":
         command_args.append("--write-subs")
-        
+
     if thumb_checkbox.get() == "on":
         command_args.append("--write-thumbnail")
-    
+
     if radio_var.get() == 2:
         ic(get_downloads_folder())
         ic(isinstance(get_downloads_folder(), str))
@@ -97,35 +100,35 @@ def dl_button_press():
             ic(folder_arg)
             command_args.append(folder_arg)
             ic(command_args)
-    
+
     dl_thread = Thread(target=call_yt_dlp, args=(url, *command_args))
     dl_thread.start()
-    
+
     # call_yt_dlp(url, *command_args)  Commenting out to test threads
-    
-    
+
+
 def checkbox_event():
     """
     This function should not be run directly.
-    
+
     Assign this function as a parameter to a ctk checkbox object.
     """
-    
+
     ic(sub_checkbox.get())
     ic(thumb_checkbox.get())
-    
 
 
 def radiobutton_event():
     """
     This function should not be run directly.
-    
+
     Assign this function as a parameter to a ctk radiobutton object.
-    
+
     """
     ic("Radio toggled.  Current value: ", radio_var.get())
 
-download_folder = get_downloads_folder() # can move this inside function
+
+download_folder = get_downloads_folder()  # can move this inside function
 
 # Set up basic properties for ctk object
 ctk.set_appearance_mode("dark")
@@ -134,7 +137,7 @@ app = ctk.CTk()
 app.title("video-dl")
 app.geometry("800x500")
 app.resizable(False, False)
-app.grid_columnconfigure((0,1,2,3,4), weight=1)
+app.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
 
 # Input box for video URL
 video_url = ctk.CTkTextbox(app, width=700, height=30, activate_scrollbars=False)
@@ -144,7 +147,7 @@ video_url.grid(sticky="w", row=0, column=0, padx=20, pady=20)
 
 # Frame for checkboxes
 app.checkbox_frame = ctk.CTkFrame(app)
-app.checkbox_frame.grid(row=100, column = 0, padx=10, pady=(10,0), sticky="w")
+app.checkbox_frame.grid(row=100, column=0, padx=10, pady=(10, 0), sticky="w")
 
 # Subtitle checkbox
 subtitle_check = ctk.StringVar(value="on")
@@ -154,7 +157,7 @@ sub_checkbox = ctk.CTkCheckBox(
     command=checkbox_event,
     variable=subtitle_check,
     onvalue="on",
-    offvalue="off"
+    offvalue="off",
 )
 sub_checkbox.grid(sticky="w", row=100, column=0, padx=5)
 
@@ -166,9 +169,9 @@ thumb_checkbox = ctk.CTkCheckBox(
     command=checkbox_event,
     variable=thumb_check,
     onvalue="on",
-    offvalue="off"
+    offvalue="off",
 )
-thumb_checkbox.grid(sticky="w",row=100, column=1, padx=5)
+thumb_checkbox.grid(sticky="w", row=100, column=1, padx=5)
 
 
 # Radio buttons for download location
@@ -181,20 +184,20 @@ radio_label.grid(sticky="w", padx=20, pady=20, row=0)
 radio_var = ctk.IntVar(value=1)
 
 radio_current_folder = ctk.CTkRadioButton(
-    radio_frame, 
+    radio_frame,
     text="Current Folder",
     command=radiobutton_event,
     variable=radio_var,
-    value=1
+    value=1,
 )
 radio_current_folder.grid(sticky="w", padx=20, pady=20, row=0, column=150)
 
 radio_downloads = ctk.CTkRadioButton(
-    radio_frame, 
+    radio_frame,
     text="Downloads folder",
     command=radiobutton_event,
     variable=radio_var,
-    value=2
+    value=2,
 )
 radio_downloads.grid(sticky="w", padx=20, pady=20, row=0, column=300)
 
@@ -232,12 +235,11 @@ output_window = ctk.CTkTextbox(
     height=150,
 )
 output_window.insert(
-    "0.0", 
+    "0.0",
     "Output will be shown here.  It will only show up after the download "
-    "has finished (or failed to download)."
+    "has finished (or failed to download).",
 )
 output_window.grid(sticky="w", column=0, padx=20, pady=20)
-
 
 
 app.mainloop()
